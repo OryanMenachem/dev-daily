f5e78d5e0160e1af79d39047b6256eadd817feb9
OryanMenachem <198452821+OryanMenachem@users.noreply.github.com>
2026-09-11 18:18:39 +0300

    Update index.js

--- content/index.js
+++ content/index.js
@@ -1,1 +1,13 @@
+window.COURSES = [
+  "sqlserver"
+];
 
+window.getCourse = function (id) {
+  return window.CONTENT_PACKS[id];
+};
+
+window.getAllCourses = function () {
+  return window.COURSES
+    .map(function (id) { return window.CONTENT_PACKS[id]; })
+    .filter(Boolean);
+};
